22a897450fe6d2628e363dbc79f49a24b1b529f6
Pesendorfer90-Mobil <pesendorfer.gerald@hotmail.com>
2024-09-19 21:38:27 +0200

    add ngx-translate, add translate Service, add data.ts for Translate Service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
 import { FormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,6 +24,14 @@ export const appConfig: ApplicationConfig = {
       timeOut: 10000, // 10 Sekunden Timeout für alle Toasts
       positionClass: 'toast-bottom-center', // Positionierung nach Wunsch
       preventDuplicates: true,
-    }))
+    })),
+    importProvidersFrom(TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+    ),
   ]
 };
